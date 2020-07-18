89bede709175bef6530151301458d1630a458f01
5377 <pradipamate07@gmail.com>
2020-07-18 20:44:44 +0530

    Updates

---
+++ precache-manifest.fcd13137f6316fb7e6bff576080b0f9c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "73a3622587d2bc6c3d50",
+    "url": "/Taskbooks/static/css/main.7aa2575c.chunk.css"
+  },
+  {
+    "revision": "73a3622587d2bc6c3d50",
+    "url": "/Taskbooks/static/js/main.72fe3c8e.chunk.js"
+  },
+  {
+    "revision": "5a545d34016468899379",
+    "url": "/Taskbooks/static/js/runtime~main.7adb55f7.js"
+  },
+  {
+    "revision": "917ed78aad354f830828",
+    "url": "/Taskbooks/static/css/2.f1b22d3c.chunk.css"
+  },
+  {
+    "revision": "917ed78aad354f830828",
+    "url": "/Taskbooks/static/js/2.e50766da.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Taskbooks/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Taskbooks/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Taskbooks/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Taskbooks/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Taskbooks/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "c57d6cbe2f7911b643250ea4c43f7ecd",
+    "url": "/Taskbooks/index.html"
+  }
+];
